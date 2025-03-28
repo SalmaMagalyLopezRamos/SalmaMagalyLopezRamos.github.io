60f03379c5c0aa48a6d71f8d375b4ba228c26348
SalmaMagalyLopezRamos <l21070029@cdmadero.tecnm.mx>
2025-03-27 20:18:51 -0600

    Add files via upload

---
+++ Nosotros.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Este archivo se puede utilizar para futuras interacciones en la página
+    // Ejemplo: Animaciones, validaciones, interacciones con elementos de la página
+
+    // Puedes agregar funciones interactivas aquí si es necesario en el futuro.
+});
